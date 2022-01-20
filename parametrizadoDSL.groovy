031fee5004b7ed430edfb245f514d6e15dbe4015
JesusCG91 <46775810+JesusCG91@users.noreply.github.com>
2022-01-20 11:09:53 +0100

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,4 +1,4 @@
-job('ejemplo-job-DSL') {
+job('ejemplo-job-DSL2') {
   description('Job DSL de ejemplo para el curso de Jenkins')
   scm {
     git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
